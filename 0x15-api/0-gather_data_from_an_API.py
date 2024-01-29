fd499f0ce23c7aee97bd1441ec47ebc299a1a3b7
sarachafikidrissi <chafikidrissisara@gmail.com>
2024-01-29 12:23:25 -0800

    task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+This module use API to return information
+of a given employee
+"""
+import json
+import sys
+import urllib.request
+
+
+if __name__ == '__main__':
+    _id = sys.argv[1]
+    url_1 = "https://jsonplaceholder.typicode.com/users/{}".format(_id)
+    url_2 = "https://jsonplaceholder.typicode.com/todos?userId={}".format(_id)
+
+    response = urllib.request.urlopen(url_1)
+    data = response.read()
+    decoded_data = data.decode('utf-8')
+    json_data = json.loads(decoded_data)
+
+    name = json_data.get('name')
+
+    print("Employee {} is done with tasks".format(name), end="")
+
+    response = urllib.request.urlopen(url_2)
+    data = response.read()
+    decoded_data = data.decode('utf-8')
+    tasks = json.loads(decoded_data)
+
+    done = 0
+    done_tasks = []
+
+    for task in tasks:
+        if task.get('completed'):
+            done_tasks.append(task)
+            done += 1
+
+    print("({}/{}):".format(done, len(tasks)))
+
+    for n in done_tasks:
+        print(n.get('title'), end="\n")
